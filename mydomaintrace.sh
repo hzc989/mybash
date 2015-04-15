0d09210ee5e9df3745138c779b910890e88fce3a
Houston Wong <hzc989@outlook.com>
2015-04-15 15:48:42 +0800

    modified

--- mydomaintrace.sh
+++ mydomaintrace.sh
@@ -8,21 +8,28 @@
 #########################################################################
 domain=$1
 if [ x$domain != x ];then
+	#do the domain name departure
+	if [ "x${domain##*.}" == "x" ];then
+		domain=${domain%*.} #remove the root. from the domain	
+	fi
+	TLD=${domain##*.}
+	REST=${domain%.*}
+	SLD="${REST##*.}.${domain##*.}"
+			
 	#get root DNS Server
 	echo -e "-------getting root DNS Server:-------\n"
-	curl http://ftp.internic.net/domain/named.cache | grep '^\.'
-	echo $root
-	#get com. DNS Server 
-	rd=`curl http://ftp.internic.net/domain/named.cache | grep '^\.' | awk '{print $4}' | sed -n '1p'`
-	echo -e "-------getting com DNS Server:-------\n"
-	dig com @$rd | grep '^com'
-	#get *.com DNS Server
-	second=`dig com @$rd | grep '^com\.' | awk '{print $5}' | sed -n '2p'`
-	echo -e "-------getting ${domain#*.*.} DNS Server:--------\n"
-	dig ${domain#*.*.} @$second | grep 'NS'
-	#get the result
-	last=`dig ${domain#*.*.} @$second | grep 'NS' | awk '{print $5}' | sed -n '2p'`
-	echo -e "-------getting result-------\n"
+	curl http://ftp.internic.net/domain/named.cache | grep "^\."
+	#get the first line of the TLD DNS Server as rd
+	rd=`curl http://ftp.internic.net/domain/named.cache | grep "^\." | awk '{print $4}' | sed -n '1p'`
+	echo -e "-------getting $TLD DNS Server:-------\n"
+	dig $TLD @$rd | grep "^$TLD"
+	#get the first line of the SLD DNS Server as second
+	second=`dig $TLD @$rd | grep "^$TLD" | awk '{print $5}' | sed -n '1p'`
+	echo -e "-------getting $SLD DNS Server:--------\n"
+	dig $SLD @$second | grep "^$SLD"
+	#get the first line of  the last DNS Server as last
+	last=`dig $SLD @$second | grep "^$SLD" | awk '{print $5}' | sed -n '1p'`
+	echo -e "-------getting $domain DNS Server-------\n"
 	dig $domain @$last | sed -n '/;; ANSWER SECTION:/,+3p'
 else
 	usage="USAGE:./mydomaintrace.sh domain"
