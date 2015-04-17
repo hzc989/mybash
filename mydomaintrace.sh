f0d82de3da43f76e26845c6a550cbab3d9a452f6
Houston Wong <hzc989@outlook.com>
2015-04-17 15:20:01 +0800

    last modified

--- mydomaintrace.sh
+++ mydomaintrace.sh
@@ -7,30 +7,38 @@
 # USAGE: this script is used to trace domain name.
 #########################################################################
 domain=$1
-if [ x$domain != x ];then
+if [ "x$domain" != "x" ];then
 	#do the domain name departure
 	if [ "x${domain##*.}" == "x" ];then
 		domain=${domain%*.} #remove the root. from the domain	
-	fi
-	TLD=${domain##*.}
-	REST=${domain%.*}
-	SLD="${REST##*.}.${domain##*.}"
-			
-	#get root DNS Server
+	fi			
+	#start getting Root DNS Server
 	echo -e "-------getting root DNS Server:-------\n"
 	curl http://ftp.internic.net/domain/named.cache | grep "^\."
-	#get the first line of the TLD DNS Server as rd
-	rd=`curl http://ftp.internic.net/domain/named.cache | grep "^\." | awk '{print $4}' | sed -n '1p'`
-	echo -e "-------getting $TLD DNS Server:-------\n"
-	dig $TLD @$rd | grep "^$TLD"
-	#get the first line of the SLD DNS Server as second
-	second=`dig $TLD @$rd | grep "^$TLD" | awk '{print $5}' | sed -n '1p'`
-	echo -e "-------getting $SLD DNS Server:--------\n"
-	dig $SLD @$second | grep "^$SLD"
-	#get the first line of  the last DNS Server as last
-	last=`dig $SLD @$second | grep "^$SLD" | awk '{print $5}' | sed -n '1p'`
-	echo -e "-------getting $domain DNS Server-------\n"
-	dig $domain @$last | sed -n '/;; ANSWER SECTION:/,+3p'
+	#get the first line of the root DNS Server as root server
+	root=`curl http://ftp.internic.net/domain/named.cache | grep "^\." | awk '{print $4}' | sed -n '1p'`
+	echo $root	
+	#init the parameters 
+	current=${domain##*.}
+	rest=${domain%.*}
+	dns=$root
+	#recursively getting the dns server
+	while [ "$current" != "${domain%%.*}.$domain" ]
+	do
+	echo -e "\n-------getting $current DNS Server-------\n"
+	if [ "$current" == "$domain" ];then
+		dig $current @$dns | grep -A 5 "^$current" | grep -v "^;;" 
+	else
+		dig $current @$dns | grep "^$current"
+	fi
+	#get the first line of the DNS Servers as the next dns server
+	type=`dig $current @$dns | grep "^$current" | awk '{print $4}' | sed -n '1p'`
+	if [ "$type" == "NS" ];then
+	dns=`dig $current @$dns | grep "^$current" | awk '{print $5}' | sed -n '1p'`
+	fi
+	current="${rest##*.}.$current"
+	rest=${rest%.*}
+	done
 else
 	usage="USAGE:./mydomaintrace.sh domain"
 	echo -e "$usage\n"
