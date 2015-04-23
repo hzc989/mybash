2841183b5aed3c394694f5a2602ab668a2b415f1
Houston Wong <hzc989@outlook.com>
2015-04-23 19:58:03 +0800

    check route with mtr

---
+++ checkrt.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#########################################################################
+# File Name: checkrt.sh
+# Author: Houston Wong
+# mail: hzc989@163.com
+# Created Time: Wed 22 Apr 2015 08:23:58 PM HKT
+#########################################################################
+ip="${1-"220.181.30.53"}"
+#using mtr to check route
+echo -e "[`date`]START checking route to $ip\n\
+No.hop IpAddr Loss% Snt Last Avg Best Wrst StDev" >> /var/log/checkrt.log
+mtr -n --report -c 100 $ip | sed '1d' | while read LINE
+do
+loss=`echo $LINE | awk '{split($0,a," ");print a[3]}'`
+avg=`echo $LINE | awk '{split($0,a," ");print a[6]}'`
+
+#filter 100%loss route
+if [ "$loss" == "sed" ];then
+	echo $LINE | sed 's/awk sed/unkown/' >> /var/log/checkrt.log
+	continue
+fi
+if [ ${loss%.*} -ge 10 -o ${avg%.*} -ge 50 ];then
+	echo $LINE >> /var/log/checkrt.log
+fi
+done
+echo "[`date`]checking route to $ip DONE" >> /var/log/checkrt.log
