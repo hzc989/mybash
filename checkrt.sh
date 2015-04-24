56964551f8e83b3ffc34640f787d529b9114600b
Houston Wong <hzc989@outlook.com>
2015-04-24 15:21:54 +0800

    last modified

--- checkrt.sh
+++ checkrt.sh
@@ -4,6 +4,8 @@
 # Author: Houston Wong
 # mail: hzc989@163.com
 # Created Time: Wed 22 Apr 2015 08:23:58 PM HKT
+# USAGE: checkrt.sh [ip|domain].default ip is 220.181.30.53
+# EXAMPLE: checkrt.sh www.163.com
 #########################################################################
 ip="${1-"220.181.30.53"}"
 #using mtr to check route
